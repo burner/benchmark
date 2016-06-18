837387552fa9951f910c11553ce2007763212e50
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2016-06-18 17:02:14 +0200

    moving it up to phobos PR head

--- source/std/experimental/randomized_unittest_benchmark.d
+++ source/std/experimental/randomized_unittest_benchmark.d
@@ -875,9 +875,10 @@ void doNotOptimizeAway(T...)(ref T t)
 }
 
 private void doNotOptimizeAwayImpl(void* p) {
-	import core.thread : getpid;
-	import std.stdio : writeln;
-	if(getpid() == 1) {
-		writeln(*cast(char*)p);
-	}
+    import core.thread : getpid;
+    import std.stdio : writeln;
+
+    if(getpid() == 0) {
+        writeln(p);
+    }
 }
