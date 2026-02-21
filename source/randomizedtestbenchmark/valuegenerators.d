6f23fa8d429a3436d93e68a321240551c91de52e
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2026-02-21 13:50:11 +0100

    array value generators

--- source/randomizedtestbenchmark/valuegenerators.d
+++ source/randomizedtestbenchmark/valuegenerators.d
@@ -1,6 +1,6 @@
 module randomizedtestbenchmark.valuegenerators;
 
-import std.traits : isSomeChar, isFloatingPoint, isNumeric, isSomeString;
+import std.traits : isSomeChar, isFloatingPoint, isNumeric, isSomeString, isIntegral;
 import std.random : Random, uniform;
 
 /* Return $(D true) if the passed $(D T) is a $(D Gen) struct.
@@ -43,8 +43,8 @@ template isGen(T)
     static assert(isGen!(Gen!(float, 0, 10)));
     static assert(isGen!(Gen!(string)));
     static assert(isGen!(Gen!(string, 0, 10)));
-    static assert(isGen!(GenASCIIString!string));
-    static assert(isGen!(GenASCIIString!(string, 0, 10)));
+    static assert(isGen!(Gen!(string, 0, 10, false)));
+    static assert(isGen!(Gen!(string, 0, 10, true)));
 }
 
 /** A $(D Gen) type that generates character values. */
@@ -78,6 +78,165 @@ struct Gen(T, T low = 0, T high = T.max) if (isNumeric!T)
     alias value this;
 }
 
+/** A $(D Gen) type that generates a fixed-size array of numeric values
+and provides slices with controlled sizes for benchmarking.
+
+The first four calls to $(D gen) return predictable slices:
+- 1st call: empty slice
+- 2nd call: 1 element
+- 3rd call: 2 elements  
+- 4th call: all elements
+- 5th+: random slice within bounds
+
+Template parameters:
+- $(D T): element type - numeric (integral/floating point), string, or bool
+- $(D N): array size (default 16384)
+- $(D lowVal): minimum element value for numeric types (default 0)
+- $(D highVal): maximum element value for numeric types (default T.max)
+- $(D strLenLow): minimum string length for string arrays (default 0)
+- $(D strLenHigh): maximum string length for string arrays (default 30)
+- $(D unicode): generate unicode strings for string arrays (default false)
+*/
+struct GenArray(T, size_t N = 16384, T lowVal = 0, T highVal = T.max) 
+    if (isIntegral!T || isFloatingPoint!T || is(T == bool))
+{
+    alias Type = T[];
+    T[N] data;
+    T[] slice;
+    size_t iteration;
+
+    void gen(ref Random rnd)
+    {
+        static assert(lowVal <= highVal);
+        
+        static if (is(T == bool))
+        {
+            foreach (ref elem; data)
+                elem = cast(bool) uniform!"[]"(0, 1, rnd);
+        }
+        else
+        {
+            foreach (ref elem; data)
+                elem = uniform!("[]")(lowVal, highVal, rnd);
+        }
+
+        setSlice(rnd);
+    }
+
+    private void setSlice(ref Random rnd)
+    {
+        iteration++;
+        if (iteration == 1)
+            slice = data[0 .. 0];
+        else if (iteration == 2)
+            slice = data[0 .. 1];
+        else if (iteration == 3)
+            slice = data[0 .. 2];
+        else if (iteration == 4)
+            slice = data[0 .. N];
+        else
+        {
+            size_t start = uniform!("[)")(0, N, rnd);
+            size_t end = uniform!("[]")(start, N - 1, rnd);
+            slice = data[start .. end + 1];
+        }
+    }
+
+    alias slice this;
+}
+
+/// Overload for string arrays
+struct GenArray(T, size_t N = 16384, size_t strLenLow = 0, size_t strLenHigh = 30, bool unicode = false) 
+    if (isSomeString!T)
+{
+    import std.algorithm : count;
+
+    alias Type = T[];
+    static if (unicode)
+        static immutable T charSet = genCharSet!T();
+    else
+        static immutable T charSet = genCharSetASCII!T();
+    static immutable size_t numCharsInCharSet = count(charSet);
+
+    T[N] data;
+    T[] slice;
+    size_t iteration;
+
+    void gen(ref Random rnd)
+    {
+        static assert(strLenLow <= strLenHigh);
+        import std.array : appender;
+
+        foreach (ref str; data)
+        {
+            auto app = appender!T();
+            app.reserve(strLenHigh);
+            size_t numElems = uniform!("[]")(strLenLow, strLenHigh, rnd);
+
+            for (size_t i = 0; i < numElems; ++i)
+            {
+                size_t toSelect = uniform!("[)")(0, numCharsInCharSet, rnd);
+                static if (unicode)
+                {
+                    import std.range : drop;
+                    import std.utf : byDchar;
+                    app.put(charSet.byDchar().drop(toSelect).front);
+                }
+                else
+                {
+                    app.put(charSet[toSelect]);
+                }
+            }
+            str = app.data;
+        }
+
+        iteration++;
+        if (iteration == 1)
+            slice = data[0 .. 0];
+        else if (iteration == 2)
+            slice = data[0 .. 1];
+        else if (iteration == 3)
+            slice = data[0 .. 2];
+        else if (iteration == 4)
+            slice = data[0 .. N];
+        else
+        {
+            size_t start = uniform!("[)")(0, N, rnd);
+            size_t end = uniform!("[]")(start, N - 1, rnd);
+            slice = data[start .. end + 1];
+        }
+    }
+
+    alias slice this;
+}
+
+unittest
+{
+    auto rnd = Random(1337);
+    
+    GenArray!(int, 100, 0, 10) gen;
+    static assert(isGen!(typeof(gen)));
+    
+    gen.gen(rnd);
+    assert(gen.slice.length == 0);
+    
+    gen.gen(rnd);
+    assert(gen.slice.length == 1);
+    
+    gen.gen(rnd);
+    assert(gen.slice.length == 2);
+    
+    gen.gen(rnd);
+    assert(gen.slice.length == 100);
+    
+    foreach (i; 0 .. 100)
+    {
+        gen.gen(rnd);
+        assert(gen.slice.length >= 1);
+        assert(gen.slice.length <= 100);
+    }
+}
+
 private pure @safe T genCharSet(T)()
 {
     import std.range : iota, chain;
@@ -89,25 +248,49 @@ private pure @safe T genCharSet(T)()
             iota(0xA1, 0x1EF).map!(a => to!T(cast(dchar) a))).joiner.array);
 }
 
-/** A $(D Gen) type that generates unicode strings with a number of
-charatacters that is between template parameter $(D low) and $(D high).
+private pure @safe T genCharSetASCII(T)()
+{
+    import std.range : chain, iota;
+    import std.algorithm : map, joiner;
+    import std.conv : to;
+    import std.array : array;
+
+    auto charSet = to!T(chain(iota(0x21, 0x7B)
+            .map!(a => to!char(cast(dchar) a)).array)
+        );
+    return charSet;
+}
+
+/** A $(D Gen) type that generates strings with a number of characters between
+template parameters $(D low) and $(D high).
+
+Template parameters:
+- $(D T): string type (string, wstring, dstring)
+- $(D low): minimum string length (default 0)
+- $(D high): maximum string length (default 30)
+- $(D unicode): if true, generate unicode strings; if false, ASCII only (default false)
 */
-struct Gen(T, size_t low = 0, size_t high = 30) if (isSomeString!T)
+struct Gen(T, size_t low = 0, size_t high = 30, bool unicode = false) if (isSomeString!T)
 {
     import std.algorithm : count;
 
     alias Type = T;
     T value;
 
-    static immutable T charSet = genCharSet!T();
+    static if (unicode)
+    {
+        static immutable T charSet = genCharSet!T();
+    }
+    else
+    {
+        static immutable T charSet = genCharSetASCII!T();
+    }
     static immutable size_t numCharsInCharSet = count(charSet);
 
     void gen(ref Random gen)
     {
         static assert(low <= high);
-        import std.range : drop;
-        import std.array : front, appender;
-        import std.utf : byDchar;
+        import std.array : appender;
 
         auto app = appender!T();
         app.reserve(high);
@@ -116,7 +299,16 @@ struct Gen(T, size_t low = 0, size_t high = 30) if (isSomeString!T)
         for (size_t i = 0; i < numElems; ++i)
         {
             size_t toSelect = uniform!("[)")(0, numCharsInCharSet, gen);
-            app.put(charSet.byDchar().drop(toSelect).front);
+            static if (unicode)
+            {
+                import std.range : drop;
+                import std.utf : byDchar;
+                app.put(charSet.byDchar().drop(toSelect).front);
+            }
+            else
+            {
+                app.put(charSet[toSelect]);
+            }
         }
 
         this.value = app.data;
@@ -140,7 +332,7 @@ struct Gen(T, size_t low = 0, size_t high = 30) if (isSomeString!T)
         {
             foreach (H; aliasSeqOf!(iota(L, 2)))
             {
-                Gen!(T, L, H) a;
+                Gen!(T, L, H, false) a;
                 a.gen(r);
                 if (L)
                 {
@@ -148,74 +340,6 @@ struct Gen(T, size_t low = 0, size_t high = 30) if (isSomeString!T)
                 }
             }
         }
-    }
-}
-
-private pure @safe T genCharSetASCII(T)()
-{
-    import std.range : chain, iota;
-    import std.algorithm : map, joiner;
-    import std.conv : to;
-    import std.array : array;
-
-    auto charSet = to!T(chain(iota(0x21, 0x7B)
-			.map!(a => to!char(cast(dchar) a)).array)
-		);
-    return charSet;
-}
-
-/** DITTO The random $(D string)s generated by this $(D Gen) only consisting 
-of ASCII character.
-*/
-struct GenASCIIString(T, size_t low = 0, size_t high = 30) if (isSomeString!T)
-{
-    import std.algorithm : count;
-
-    alias Type = T;
-
-    static immutable Type charSet = genCharSet!T();
-    static immutable size_t numCharsInCharSet = count(charSet);
-
-    Type value;
-
-    void gen(ref Random gen) pure @safe
-    {
-        import std.array : appender;
-
-        auto app = appender!Type();
-        app.reserve(high);
-        size_t numElems = uniform!("[]")(low, high, gen);
-
-        for (size_t i = 0; i < numElems; ++i)
-        {
-            size_t toSelect = uniform!("[)")(0, numCharsInCharSet, gen);
-            app.put(charSet[toSelect]);
-        }
-
-        this.value = app.data;
-    }
-
-    alias value this;
-}
-
-@safe pure unittest
-{
-    import std.utf : validate;
-    import std.array : empty;
-    import std.exception : assertNotThrown;
-    import std.meta : AliasSeq;
-
-    auto rnd = Random(1337);
-
-    foreach (T; AliasSeq!(string, wstring, dstring))
-    {
-        GenASCIIString!(T, 5, 5) gen;
-        static assert(isGen!(typeof(gen)));
-        gen.gen(rnd);
-        T str = gen;
-
-        assert(!str.empty);
-        //assertNotThrown(validate(str)); TODO fix the exception
     }
 }
 
@@ -317,18 +441,31 @@ already a $(D Gen) or no $(D Gen) for given $(D T) is available.
 */
 template ParameterToGen(T)
 {
-    import std.traits : isSomeChar, isIntegral, isFloatingPoint, isSomeString;
+    import std.traits : isSomeChar, isIntegral, isFloatingPoint, isSomeString, isArray;
 
     static if (isGen!T)
         alias ParameterToGen = T;
+    else static if (isSomeString!T)
+        alias ParameterToGen = Gen!(T, 0, 32);
+    else static if (is(T == U[], U))
+    {
+        static if (is(U == bool))
+            alias ParameterToGen = GenArray!bool;
+        else static if (isIntegral!U)
+            alias ParameterToGen = GenArray!U;
+        else static if (isFloatingPoint!U)
+            alias ParameterToGen = GenArray!U;
+        else static if (isSomeString!U)
+            alias ParameterToGen = GenArray!U;
+        else
+            static assert(false, T.stringof);
+    }
     else static if (isIntegral!T)
         alias ParameterToGen = Gen!(T, T.min, T.max);
     else static if (isSomeChar!T)
         alias ParameterToGen = Gen!(T);
     else static if (isFloatingPoint!T)
         alias ParameterToGen = Gen!(T, T.min_normal, T.max);
-    else static if (isSomeString!T)
-        alias ParameterToGen = Gen!(T, 0, 32);
     else static if (is(T : GenASCIIString!(S), S...))
         alias ParameterToGen = T;
     else
@@ -359,5 +496,161 @@ template ParameterToGen(T)
     {
         alias TP = staticMap!(ParameterToGen, T);
         static assert(isGen!TP);
+    }
+}
+
+@safe pure unittest
+{
+    import std.meta : AliasSeq;
+
+    foreach (T; AliasSeq!(int[], float[], double[], long[]))
+    {
+        alias GenArr = ParameterToGen!T;
+        static assert(isGen!GenArr);
+        static assert(is(GenArr.Type == T));
+    }
+}
+
+@safe pure unittest
+{
+    auto rnd = Random(1337);
+
+    GenArray!(string, 10, 3, 5, false) gen;
+    static assert(isGen!(typeof(gen)));
+    static assert(is(typeof(gen.slice) == string[]));
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 0);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 1);
+    assert(gen[0].length >= 3 && gen[0].length <= 5);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 2);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 10);
+
+    foreach (i; 0 .. 10)
+    {
+        gen.gen(rnd);
+        assert(gen.slice.length >= 1);
+        assert(gen.slice.length <= 10);
+    }
+}
+
+@safe pure unittest
+{
+    import std.meta : AliasSeq;
+
+    foreach (T; AliasSeq!(string[], wstring[], dstring[]))
+    {
+        alias GenArr = ParameterToGen!T;
+        static assert(isGen!GenArr);
+        static assert(is(GenArr.Type == T));
+    }
+}
+
+@safe pure unittest
+{
+    auto rnd = Random(1337);
+
+    GenArray!(int, 10, 0, 100) gen;
+    static assert(isGen!(typeof(gen)));
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 0);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 1);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 2);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 10);
+
+    foreach (i; 0 .. 10)
+    {
+        gen.gen(rnd);
+        assert(gen.slice.length >= 1);
+        assert(gen.slice.length <= 10);
+    }
+}
+
+@safe pure unittest
+{
+    auto rnd = Random(1337);
+
+    GenArray!(float, 10, 0.0f, 1.0f) gen;
+    static assert(isGen!(typeof(gen)));
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 0);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 1);
+    assert(gen[0] >= 0.0f && gen[0] <= 1.0f);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 2);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 10);
+}
+
+@safe pure unittest
+{
+    auto rnd = Random(1337);
+
+    GenArray!(string, 10, 3, 5, false) gen;
+    static assert(isGen!(typeof(gen)));
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 0);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 1);
+    assert(gen[0].length >= 3 && gen[0].length <= 5);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 2);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 10);
+
+    foreach (i; 0 .. 10)
+    {
+        gen.gen(rnd);
+        assert(gen.slice.length >= 1);
+        assert(gen.slice.length <= 10);
+    }
+}
+
+@safe pure unittest
+{
+    auto rnd = Random(1337);
+
+    GenArray!bool gen;
+    static assert(isGen!(typeof(gen)));
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 0);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 1);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 2);
+
+    gen.gen(rnd);
+    assert(gen.slice.length == 16384);
+
+    foreach (i; 0 .. 10)
+    {
+        gen.gen(rnd);
+        assert(gen.slice.length >= 1);
+        assert(gen.slice.length <= 16384);
     }
 }
